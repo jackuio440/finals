8d9d57a2d14b4831fa54aadeca1bbdf487d73b35
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2023-01-02 18:35:07 +0800

    Relate # ! 提交將修正名稱
    修改項目:
    
    模組:

--- news_find.py
+++ news_find.py
@@ -12,7 +12,7 @@ from gtts import gTTS
 import pyttsx3
 
 
-def newsFind():
+def news_find():
     """."""
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36'}
