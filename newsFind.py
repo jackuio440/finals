81d029e212c18d738f79cfa5ac40580133c1c8e6
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2023-01-02 17:35:06 +0800

    Relate # ! 提交將部份風格化
    修改項目:
    
    模組:

--- newsFind.py
+++ newsFind.py
@@ -1,3 +1,4 @@
+"""."""
 from __future__ import print_function
 from turtle import title
 from bs4 import BeautifulSoup
@@ -11,9 +12,8 @@ from gtts import gTTS
 import pyttsx3
 
 
-
-
 def newsFind():
+    """."""
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36'}
     # 目標網站 所有新聞標題與連結
@@ -31,14 +31,14 @@ def newsFind():
         articles.append(
             root + link + '.html'
         )
-        
+
     # 如果該文件已存在則打開文件，並從開頭開始編輯，原有內容會被刪除。
     # 隨機抽取一則新聞
     url2 = articles[random.randint(0,20)]
     print(url2)
     headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36'}
-        # 目標網站
+    # 目標網站
     r = requests.get(url2, headers=headers)
     r.encoding = 'utf-8'
     sp = BeautifulSoup(r.text, 'html.parser')
@@ -55,25 +55,26 @@ def newsRead():
             data = f.read()
             engine = pyttsx3.init()
 
-
             engine.setProperty('rate', 150)
             engine.setProperty('volume', 0.9)
 
-            
             engine.say(data)
 
-            
             engine.runAndWait()
             print(data)
             print('------------------')
+
+
 def newsRead_en():
-    
+    """."""
+
     with open('news.txt', 'r') as f:
         data = f.read()
+
     def google_translator(texts):
         translator = Translator()
         return translator.translate(texts,dest="en").text
-    
+
     engine = pyttsx3.init()
     engine.setProperty('rate', 150)
     engine.setProperty('volume', 0.9)
